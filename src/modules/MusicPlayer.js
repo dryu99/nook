79a4b728d9504fa3ed19b92d65d91126bb04babd
jackyzha0 <j.zhao2k19@gmail.com>
2020-05-30 01:21:19 -0700

    updated musicplayer

--- src/modules/MusicPlayer.js
+++ src/modules/MusicPlayer.js
@@ -1,16 +1,39 @@
+// set global state
+let isPlaying = false
+let audio = undefined;
 
 // wait for page to load first 
 document.addEventListener('DOMContentLoaded', (event) => {
 
     // fetch actual audio html element from document
-    const audio = document.getElementById("audio");
-    
-    // check new time on audio finish
-    audio.onended = updateSrcWithHr();
-})
+    audio = document.getElementById("audio");
+    audio.addEventListener("ended", () => {
+        updateSrcWithHr()
+    })
 
-// set global isPlaying state
-let isPlaying = false
+    updateSrcWithHr()
+
+    // updates audio to use current hour
+    function updateSrcWithHr() {
+        const hour = getCurrentHr()
+        setAudioSrc(hour)
+    }
+
+    // returns current hour from 0 to 23
+    function getCurrentHr() {
+        const currentHour = (new Date()).getHours();
+        return currentHour
+    }
+
+    // sets audio src with appropriate hour
+    function setAudioSrc(hr) {
+        audio.pause()
+        const audioSrc = document.getElementById("audioSrc")
+        audioSrc.src = `/music/${hr}.mp3`
+        audio.load()
+        audio.play()
+    }
+})
 
 export function toggleIsPlaying() {
     if (isPlaying) {
@@ -20,24 +43,4 @@ export function toggleIsPlaying() {
     }
 
     isPlaying = !isPlaying
-}
-
-// updates audio to use current hour
-function updateSrcWithHr() {
-    const hour = getCurrentHr()
-    setAudioSrc(hour)
-    audio.play()
-}
-
-// returns current hour from 0 to 23
-function getCurrentHr() {
-    const currentHour = (new Date()).getHours();
-    return currentHour
-}
-
-// sets audio src with appropriate hour
-function setAudioSrc(hr) {
-    const audioSrc = document.getElementById("audioSrc")
-    audioSrc.src = `/music/${hr}.mp3`
-    audio.load()
 }
