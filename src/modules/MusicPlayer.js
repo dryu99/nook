cf9f3a1412631c71f638a506d0722013aa78d3d3
Daniel Ryu <rujjiryu99@gmail.com>
2020-05-30 20:10:22 -0700

    restructure music files and handle new songs in music player

--- src/modules/MusicPlayer.js
+++ src/modules/MusicPlayer.js
@@ -4,6 +4,7 @@ import { getCurrentHr} from './Time'
 let isPlaying = false
 let audio = undefined;
 const playButton = document.getElementById("togglePlay")
+let songCache = [];
 
 // fetch actual audio html element from document
 audio = document.getElementById("audio");
@@ -20,23 +21,42 @@ audio.addEventListener("timeupdate", () => {
     document.getElementById("completed").style.flexBasis = percent * 100 + '%'
 })
 
+// sets new audio src if current one doesn't exist
+audio.addEventListener('error', function(e) {
+    updateSrcWithHr();
+}, true);
+
 // updates audio to use current hour
 function updateSrcWithHr() {
     const hour = getCurrentHr()
-    setAudioSrc(hour)
+    const newSongId = findNewSongId(hour);
+    setAudioSrc(hour, newSongId)
 } 
 
-// sets audio src with appropriate hour
-function setAudioSrc(hr) {
+// sets audio src with appropriate hour and song id
+function setAudioSrc(hr, songId) {
     audio.pause()
     const audioSrc = document.getElementById("audioSrc")
-    audioSrc.src = `music/${hr}.mp3`
-    audio.load()
+    audioSrc.src = `music/${hr}/${songId}.mp3`
+    audio.load()       
+}
+
+// generates new song id given the hour - format is ${hr}.${num btw 1-10}
+function findNewSongId(hr) {
+    let lastSongPlayedId = songCache[hr];
+    let randomSongId = Math.ceil(Math.random() * 10)
+    // don't play last played song num
+    while (lastSongPlayedId && lastSongPlayedId === randomSongId) {
+        randomSongId = Math.ceil(Math.random() * 10)
+    }
+
+    songCache[hr] = randomSongId;
+    return hr + "." + randomSongId;
 }
 
 updateSrcWithHr()
 
-export function toggleIsPlaying() {
+export function toggleIsPlaying() {  
     if (isPlaying) {
         playButton.innerText = "►"
         audio.pause()
