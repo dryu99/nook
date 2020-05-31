f31746cb1f2551b7186384121e8cf15e1d54cf4d
jackyzha0 <j.zhao2k19@gmail.com>
2020-05-30 20:12:46 -0700

    add more granularity

--- src/modules/UpdateTime.js
+++ src/modules/UpdateTime.js
@@ -1,9 +1,13 @@
 const time = document.getElementById('timeDisplay');
+const gradient = document.getElementById('gradient');
 
 function updateTime() {
     const now = new Date();
     const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',hour12: true }).replace("AM", "").replace("PM", "")
     time.innerText = timeString
+
+    const hours = now.getHours() + (now.getMinutes() / 60) + (now.getSeconds() / 3600)
+    gradient.style.transform = `translateY(-${hours * 100}vh)`
 }
 
 export function startUpdatingClock() {
